e8f17b0e3299e2a9175f358f02e9eb140f1a0d90
Vanessa O <vaneolipa@gmail.com>
2023-03-10 12:49:24 -0500

    create DOMContentLoaded event

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import { getCategories } from './modules/functions.js';
+
+document.addEventListener('DOMContentLoaded', getCategories);
