b40272b8ace013673a89495891b2caab54cb7e2b
Vanessa O <vaneolipa@gmail.com>
2023-03-10 14:24:32 -0500

    create functions: selectCategory and showRecipes

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,4 +1,4 @@
-import { selectCategories } from './variables.js';
+import { selectCategories, result } from './variables.js';
 
 export const showCategories = (categories) => {
   categories.forEach(category => {
@@ -15,5 +15,54 @@ export const getCategories = () => {
   fetch(url)
     .then( response => response.json())
     .then(data => showCategories(data.categories))
+    .catch(error => error);
+}
+
+export const showRecipes = (recipes) => {
+  recipes.forEach(recipe => {
+    const { strMeal, strMealThumb, idMeal } = recipe;
+    
+    const recipeContainer = document.createElement('DIV');
+    recipeContainer.classList.add('col-md-4');
+
+    const recipeCard = document.createElement('DIV');
+    recipeCard.classList.add('card', 'mb-4');
+
+    const recipeImg = document.createElement('IMG');
+    recipeImg.classList.add('card-img-top');
+    recipeImg.alt = `Recipe Image ${strMeal}`;
+    recipeImg.src = strMealThumb;
+
+    const recipeCardBody = document.createElement('DIV');
+    recipeCardBody.classList.add('card-body');
+
+    const recipeHeading = document.createElement('H3');
+    recipeHeading.classList.add('card-title', 'mb-3');
+    recipeHeading.textContent = strMeal;
+
+    const seeRecipeBtn = document.createElement('BUTTON');
+    seeRecipeBtn.classList.add('btn', 'btn-danger', 'w-100');
+    seeRecipeBtn.textContent = 'See recipe';
+
+    recipeCardBody.appendChild(recipeHeading);
+    recipeCardBody.appendChild(seeRecipeBtn);
+
+    recipeCard.appendChild(recipeImg);
+    recipeCard.appendChild(recipeCardBody);
+
+    recipeContainer.appendChild(recipeCard);
+
+    result.appendChild(recipeContainer)
+  });
+}
+
+export const selectCategory = (e) => {
+  const category = e.target.value;
+
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+
+  fetch(url)
+    .then( response => response.json())
+    .then(data => showRecipes(data.meals))
     .catch(error => error);
 }
