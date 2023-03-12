f15bff614ecd765ce8ff3f969bce362a92cd1fe8
Vanessa O <vaneolipa@gmail.com>
2023-03-12 13:36:00 -0500

    Add events for show and hide sections, and getFavorites.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import { getCategories, selectCategory } from './modules/functions.js';
-import { selectCategories } from './modules/variables.js';
+import {
+  getCategories, selectCategory, getFavorites
+} from './modules/functions.js';
+import {
+  selectCategories, favoritesLink, homeLink, favoritesSection, homeSection
+} from './modules/variables.js';
 
-document.addEventListener('DOMContentLoaded', getCategories);
+document.addEventListener('DOMContentLoaded', () => {
+  getCategories();
+  getFavorites();
+});
 
 selectCategories.addEventListener('change', selectCategory);
+
+favoritesLink.addEventListener('click', () => {
+  favoritesSection.classList.remove('d-none');
+  homeSection.classList.add('d-none');
+})
+
+homeLink.addEventListener('click', () => {
+  favoritesSection.classList.add('d-none');
+  homeSection.classList.remove('d-none');  
+})
