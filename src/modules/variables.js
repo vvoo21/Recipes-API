38daf61b49ee2cc9c4756f1c948f0a130555f5a5
Vanessa O <vaneolipa@gmail.com>
2023-03-12 11:01:51 -0500

    Create selectors for sections and nav-link

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -1,2 +1,10 @@
 export const selectCategories = document.querySelector('#categories');
 export const result = document.querySelector('#result');
+
+// Sections
+export const favoritesSection = document.querySelector('.favorites-section');
+export const homeSection = document.querySelector('.recipes-section');
+
+// Nav links
+export const favoritesLink = document.querySelector('#favorites-link');
+export const homeLink = document.querySelector('#home-link');
